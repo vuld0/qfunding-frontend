29573d85abefd75122644342f8abaf42252525bd
vuld0 <chiragsonubablani@karunya.edu.in>
2021-10-01 21:34:55 +0530

    minor fixes

--- src/Web3Client.js
+++ src/Web3Client.js
@@ -1,6 +1,9 @@
 import Web3 from 'web3';
 import QFundingBuild from 'contracts/qfunding.json'
+
+//update adress here after deploy
 let selectedAccount;
+var qfundingAddress = "0x36855DcFF29d24DC28372BC2bD6342170a7a735a"
 
 
 export const init = async () => {
@@ -11,6 +14,7 @@ export const init = async () => {
 
         //trying connecting to the wallet
         provider.request({method: 'eth_requestAccounts'}).then((accounts) => {
+        selectedAccount = accounts[0];
           console.log(accounts);
         })
         .catch((err) => {
@@ -18,13 +22,18 @@ export const init = async () => {
         });
     //if the wallet account has been changed then console it
     window.ethereum.on('accountsChanged', function(accounts) {
-      selectedAccount = accounts[0];
+      
       console.log(`Selected account changed to ${selectedAccount}`);
     });
     }
     const web3 = new Web3(provider);
 
     const networkId = await web3.eth.net.getId();
-    const qFundingContract = new web3.eth.Contract(QFundingBuild.abi);
-    console.log(qFundingContract)
+    const qFundingContract = new web3.eth.Contract(QFundingBuild.abi, qfundingAddress);
+    console.log(qFundingContract);
+    console.log("check");
+    var poolAdress = await qFundingContract.methods.sponsorPool().send({from: selectedAccount});
+    console.log(poolAdress);
+
+   
 }
